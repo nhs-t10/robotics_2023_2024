b2e7470f569976f5841197b37976a2aa925c874f
ilan <ilansbernstein@gmail.com>
2023-09-26 19:07:36 -0400

    Added region based averaging and init to AbstractResultCvPipeline

---
+++ TeamCode/src/main/java/com/pocolifo/robobase/vision/EdgeDetection.java
@@ -0,0 +1,97 @@
+package com.pocolifo.robobase.vision;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+
+public class EdgeDetection extends AbstractResultCvPipeline<SpikePosition> {
+    static final Scalar YCRCB_MIN = new Scalar(0, 95, 190);
+    static final Scalar YCRCB_MAX = new Scalar(125, 105, 250);
+
+
+    // Working variables. Because of memory concerns, we're not allowed to make ANY non-primitive variables within the `processFrame` method.
+
+    //Mat is what you see
+    Mat YCrCb = new Mat(), redPixels = new Mat(), hierarchy = new Mat();
+    MatOfPoint biggestContour;
+
+    ArrayList<MatOfPoint> contours;
+
+    Rect biggestContourBoundingRect;
+
+    int largeBlobCenterX, inputWidth;
+
+    // Volatile since accessed by OpMode thread w/o synchronization
+    private volatile SpikePosition position = SpikePosition.LEFT;
+
+    /**
+    * @return the spike position
+    * */
+    @Override
+    public SpikePosition getResult() {
+        return position;
+    }
+
+    /**
+     * @param input the frame to be processed
+     * @return the processed frame
+     */
+    @Override
+    public Mat processFrame(Mat input)
+    {
+        //convert the input to YCrCb, which is better for analysis than the default bgr.
+        Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_BGR2YCrCb);
+
+        //filter the image to ONLY redish pixels
+        Core.inRange(YCrCb, YCRCB_MIN, YCRCB_MAX, redPixels);
+
+        //Find the biggest blob of reddish pixels.
+        //It likes using a list of Matrices of points instead of something more simple, but that's ok.
+        contours = new ArrayList<>();
+        Imgproc.findContours(redPixels, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        //FeatureManager.logger.log("Contour Count: " + contours.size());
+        //only bother continuing if there were any contours found
+        if(contours.size() == 0) return input;
+
+        double biggestArea = -1;
+        //look through the list and find the biggest contour
+        for(MatOfPoint contour : contours) {
+            double area = Imgproc.boundingRect(contour).area();
+            if(area > biggestArea) {
+                biggestArea = area;
+                biggestContour = contour;
+            }
+        }
+
+        biggestContourBoundingRect = Imgproc.boundingRect(biggestContour);
+
+        Imgproc.rectangle(
+                input, // Buffer to draw on
+                new Point(biggestContourBoundingRect.x, biggestContourBoundingRect.y), // First point which defines the rectangle
+                new Point(biggestContourBoundingRect.x + biggestContourBoundingRect.width,
+                        biggestContourBoundingRect.y + biggestContourBoundingRect.height), // Second point which defines the rectangle
+                new Scalar(0.5, 255, 0), // The color the rectangle is drawn in
+                2); // Thickness of the rectangle lines
+
+        largeBlobCenterX = biggestContourBoundingRect.x + (biggestContourBoundingRect.width / 2);
+        inputWidth = input.width();
+
+        //depending on which third the blob's center falls into, report the result position.
+        if(largeBlobCenterX < inputWidth / 3) {
+            position = SpikePosition.LEFT;
+        } else if(largeBlobCenterX < (inputWidth * 2) / 3) {
+            position = SpikePosition.CENTER;
+        } else {
+            position = SpikePosition.RIGHT;
+        }
+
+        return input;
+    }
+}
