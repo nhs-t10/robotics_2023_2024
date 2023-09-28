54569cdc7fce2871d99b51c1406be8d3cb2d4639
ilan <ilansbernstein@gmail.com>
2023-09-28 15:15:52 -0400

    Renamed hardware items in map and added WebcamTestingAuto.

---
+++ TeamCode/src/main/java/centerstage/auto/WebcamTestingAuto.java
@@ -0,0 +1,33 @@
+package centerstage.auto;
+
+import com.pocolifo.robobase.BuildProperties;
+import com.pocolifo.robobase.bootstrap.AutonomousOpMode;
+import com.pocolifo.robobase.vision.EdgeDetection;
+import com.pocolifo.robobase.vision.Webcam;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Webcam Testing " + BuildProperties.VERSION)
+public class WebcamTestingAuto extends AutonomousOpMode {
+
+    Webcam webcam;
+
+    /**
+     *
+     */
+    @Override
+    public void initialize() {
+        webcam = new Webcam(hardwareMap, "Webcam");
+
+        webcam.open(new EdgeDetection());
+//
+//        webcam.getPipeline().getResult();
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void run() {
+
+    }
+}
--- TeamCode/src/main/java/centerstage/teleop/TestingTeleOp.java
+++ TeamCode/src/main/java/centerstage/teleop/TestingTeleOp.java
@@ -1,12 +1,12 @@
 package centerstage.teleop;
 
+import static centerstage.Constants.ROBOT;
+
 import com.pocolifo.robobase.BuildProperties;
 import com.pocolifo.robobase.bootstrap.TeleOpOpMode;
 import com.pocolifo.robobase.control.GamepadCarWheels;
 import com.pocolifo.robobase.motor.CarWheels;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import static centerstage.Constants.ROBOT;
 
 @TeleOp(name = "TeleOp " + BuildProperties.VERSION)
 public class TestingTeleOp extends TeleOpOpMode {
@@ -20,11 +20,11 @@ public class TestingTeleOp extends TeleOpOpMode {
                 1120,
                 10d,
                 ROBOT,
-                "W-FL",
-                "W-FR",
-                "W-BL",
-                "W-BR",
-                "W-FL"
+                "FL",
+                "FR",
+                "BL",
+                "BR",
+                "FL"
         );
 
         this.gamepadCarWheels = new GamepadCarWheels(this.carWheels, this.gamepad1);
