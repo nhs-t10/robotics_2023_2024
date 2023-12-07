2c13010e82d50cffb4ad704c13533a9251b4c2a1
EJS70 <ejs70@students.needham.k12.ma.us>
2023-12-07 15:33:23 -0500

    created simple roadrunner auto

---
+++ TeamCode/src/main/java/centerstage/auto/RRAuto.java
@@ -0,0 +1,23 @@
+package centerstage.auto;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.pocolifo.robobase.bootstrap.AutonomousOpMode;
+import roadrunner.drive.RRInterface;
+
+public class RRAuto extends AutonomousOpMode {
+    RRInterface rrInterface;
+    Pose2d startPose;
+
+    @Override
+    public void initialize() {
+        rrInterface = new RRInterface(hardwareMap);
+        startPose = new Pose2d(0,0);
+    }
+
+    @Override
+    public void run() {
+        Trajectory trajectory = rrInterface.trajectoryBuilder(startPose).forward(5).build();
+        rrInterface.followTrajectory(trajectory);
+    }
+}
