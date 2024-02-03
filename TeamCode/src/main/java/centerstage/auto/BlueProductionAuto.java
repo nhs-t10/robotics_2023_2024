be74ebbd53c1dda1c82a40b139d3477029b9ca4a
liam <ltb271@students.needham.k12.ma.us>
2024-02-03 12:40:15 -0500

    Fixed spike detection CV

--- TeamCode/src/main/java/centerstage/auto/BlueProductionAuto.java
+++ TeamCode/src/main/java/centerstage/auto/BlueProductionAuto.java
@@ -2,13 +2,11 @@ package centerstage.auto;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.pocolifo.robobase.Alliance;
-import com.pocolifo.robobase.BuildProperties;
 import com.pocolifo.robobase.StartSide;
-import com.pocolifo.robobase.vision.NovelYCrCbDetection;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.pocolifo.robobase.vision.DynamicYCrCbDetection;
 
 public class BlueProductionAuto extends BaseProductionAuto {
     public BlueProductionAuto(StartSide startSide, Pose2d startPosition) {
-        super(new NovelYCrCbDetection(1), Alliance.BLUE, startSide, startPosition);
+        super(new DynamicYCrCbDetection(false), Alliance.BLUE, startSide, startPosition);
     }
 }
--- TeamCode/src/main/java/centerstage/auto/RedProductionAuto.java
+++ TeamCode/src/main/java/centerstage/auto/RedProductionAuto.java
@@ -2,13 +2,11 @@ package centerstage.auto;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.pocolifo.robobase.Alliance;
-import com.pocolifo.robobase.BuildProperties;
 import com.pocolifo.robobase.StartSide;
-import com.pocolifo.robobase.vision.NovelYCrCbDetection;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.pocolifo.robobase.vision.DynamicYCrCbDetection;
 
 public class RedProductionAuto extends BaseProductionAuto {
     public RedProductionAuto(StartSide startSide, Pose2d startPosition) {
-        super(new NovelYCrCbDetection(2), Alliance.RED, startSide, startPosition);
+        super(new DynamicYCrCbDetection(true), Alliance.RED, startSide, startPosition);
     }
 }
