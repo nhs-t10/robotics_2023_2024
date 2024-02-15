cf3e9120c5b1309870b488ecb67950c7038a2763
liam <ltb271@students.needham.k12.ma.us>
2024-02-15 15:02:08 -0500

    Created DistanceMovement

---
+++ TeamCode/src/main/java/com/pocolifo/robobase/DistanceMovement.java
@@ -0,0 +1,27 @@
+package com.pocolifo.robobase;
+
+import com.pocolifo.robobase.novel.NovelMecanumDrive;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+public class DistanceMovement {
+    private static double PRECISION_IN = 0.5;
+    private static double MAX_SPEED = 5;
+    private static NovelMecanumDrive movementController;
+    public static void move(Vector3D movement) {
+        Vector3D position = DeadWheels.getPosition();
+        Vector3D target = position.add(movement);
+        while (position.distance(target) > 0.5) {
+            position = DeadWheels.getPosition();
+            movementController.setVelocity(getNewVelocity(position, target));
+        }
+    }
+
+    private static Vector3D getNewVelocity(Vector3D position, Vector3D target) {
+        double horizontalMovement = target.getX() - position.getX();
+        double verticalMovement = target.getY() - position.getY();
+        double rotation = target.getZ() - position.getZ();
+
+        return new Vector3D(horizontalMovement, verticalMovement, rotation)
+                .normalize().scalarMultiply(MAX_SPEED);
+    }
+}
